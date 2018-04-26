017a7884277390f7986c00a417b41b4eca362c0a
Josu <josufmc@hotmail.com>
2018-04-26 07:01:55 +0200

    Added repository for statements

---
+++ StatementNS/Model/Statement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrueOrFalse.StatementNS.Model
+{
+    [Serializable]
+    class Statement
+    {
+        public string Text { get; set; }
+        public bool IsTrue { get; set; }
+
+        public Statement()
+        {
+
+        }
+
+        public Statement(string text, bool isTrue)
+        {
+            Text = text;
+            IsTrue = isTrue;
+        }
+    }
+}
---
+++ StatementNS/Repository/StatementXmlFileRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using TrueOrFalse.StatementNS.Model;
+
+namespace TrueOrFalse.StatementNS.Repository
+{
+    class StatementXmlFileRepository
+    {
+        public string FilePath { get; }
+        public IList<Statement> Statements { get; private set; }
+
+        public StatementXmlFileRepository(string filePath)
+        {
+            FilePath = filePath;
+            Statements = new List<Statement>();
+        }
+
+        public void Add(Statement statement)
+        {
+            Statements.Add(statement);
+        }
+
+        public void Remove(int index)
+        {
+            Statements.RemoveAt(index);
+        }
+
+        public Statement this[int index] => Statements[index];
+
+        public void Save()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Statement>));
+            Stream fileStream = new FileStream(FilePath, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fileStream, Statements);
+            fileStream.Close();
+        }
+
+        public void Load()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Statement>));
+            Stream fileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            Statements = (List<Statement>)xmlFormat.Deserialize(fileStream);
+            fileStream.Close();
+        }
+
+        public int Count => Statements.Count;
+
+
+    }
+}
